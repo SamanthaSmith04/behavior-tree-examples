9122628e3c54729a23510f01e43895d1cf446ec6
SamanthaSmith04 <Troyandme04@gmail.com>
2025-11-07 10:35:38 -0500

    Added example nodes file

---
+++ bt_application/include/demo_bt_nodes.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <atomic>
+
+// behavior tree common node headers import
+// pulls in all the common behavior tree nodes for ROS2
+#include <bt_common_ros_headers.hpp>
+
+
+namespace example_bt_nodes 
+{
+  /**
+   * Your custom behavior tree nodes can be defined here.
+   * Remember to register them with the factory in the ExampleWidget class in example_widget.cpp
+   * 
+   */
+
+   /**
+      // Example of a custom synchronous action node
+      class ExampleBTNode : public BT::SyncActionNode
+      {
+          public:
+            inline static std::string ExampleInputKey = "example_input";
+            inline static std::string ExampleOutputKey = "example_output";
+            
+            // Constructor
+            ExampleBTNode(const std::string& name, const BT::NodeConfiguration& config)
+              : BT::SyncActionNode(name, config) 
+            {
+              // Initialization code here
+            }
+
+            // This method is used to define the ports of the node
+            static BT::PortsList providedPorts()
+            {
+              return{ BT::InputPort<std::string>("example_input"),
+                      BT::OutputPort<std::string>("example_output") };
+            }
+
+            // This is the main method that is called when the node is ticked
+            BT::NodeStatus tick() override
+            {
+              // Node execution code here
+              return BT::NodeStatus::SUCCESS;
+            }
+        };
+    */
+
+}
--- bt_application/src/example_widget.cpp
+++ bt_application/src/example_widget.cpp
@@ -1,5 +1,15 @@
 #include "example_widget.hpp"
 
+// behavior tree common node headers
+#include <action_nodes.hpp>
+#include <condition_nodes.hpp>
+#include <bt_ros_nodes.hpp>
+
+// Your custom behavior tree nodes
+#include <demo_bt_nodes.hpp>
+
+#include <QTimer>
+
 #include "ui_example_gui.h" // will always be "ui_" + <ui file name> + ".h"
 
 namespace example_widget
@@ -12,9 +22,37 @@ ExampleWidget::ExampleWidget(rclcpp::Node::SharedPtr ros_node, QWidget *parent)
   // find the file for the behavior tree configuration
   std::string package_path = ament_index_cpp::get_package_share_directory("bt_application");
   std::string gui_bt_file = package_path + "/config/example_bt.xml";  
+ 
+  // Set up the QT UI components
+  ui_->setupUi(this);
 
   // initialize the UI for the GUI
-  ui_->setupUi(this);
+  blackboard = BT::Blackboard::create();
+
+  // SET Blackboard values here
+  blackboard->set("main_gui", static_cast<QWidget*>(this));
+
+  // Register custom nodes in the factory if any
+  BT::BehaviorTreeFactory factory;
+  // Example formatting to register a node:
+  // factory.registerNodeType<BT::AlwaysSuccessNode>("AlwaysSuccess");
+
+  // Example formatting to register a ROS node:
+  // auto ros_node_params = BT::RosNodeParams(ros_node, "topic_name"); 
+  // factory.registerNodeType<ROSBTNodeName>("ROSBTNodeName", ros_node_params);
+
+  // Create the Behavior Tree
+  this->tree = factory.createTreeFromFile(gui_bt_file, blackboard);
+
+  // Do any other GUI / BT setup here
+
+  // Start ticking Behavior Tree
+  QTimer *tree_timer = new QTimer(this);
+  QObject::connect(tree_timer, &QTimer::timeout, [this]() {
+      BT::NodeStatus status = this->tree.tickOnce();
+  });
+  tree_timer->start(10); // Tick every 10 ms
+
 }
 
 ExampleWidget::~ExampleWidget()
